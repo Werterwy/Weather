52a417b5c857b59082cac96b205c12eabf939300
Werterwy <127129773+Werterwy@users.noreply.github.com>
2024-04-06 16:44:15 +0500

    реализовал возможность изменение иконки для состояние погоды

--- app/src/main/java/com/example/weather/MainActivity.java
+++ app/src/main/java/com/example/weather/MainActivity.java
@@ -1,10 +1,14 @@
 package com.example.weather;
 
+import android.annotation.SuppressLint;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -15,6 +19,7 @@ import org.json.JSONObject;
 
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
@@ -23,8 +28,12 @@ public class MainActivity extends AppCompatActivity {
 
     private EditText cityEditText;
     private TextView temperatureTextView;
+
+    private ImageView weatherIconImageView;
     private Button getWeatherButton;
 
+   // private WeatherData weatherData;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,6 +42,8 @@ public class MainActivity extends AppCompatActivity {
         cityEditText = findViewById(R.id.cityEditText);
         temperatureTextView = findViewById(R.id.temperatureTextView);
         getWeatherButton = findViewById(R.id.getWeatherButton);
+
+        weatherIconImageView = findViewById(R.id.weatherIconImageView);
 
         getWeatherButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -48,10 +59,10 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    private class GetWeatherTask extends AsyncTask<String, Void, String> {
+    private class GetWeatherTask extends AsyncTask<String, Void, WeatherData> implements com.example.weather.GetWeatherTask {
 
         @Override
-        protected String doInBackground(String... params) {
+        protected WeatherData doInBackground(String... params) {
             String city = params[0];
             String apiKey = "0ec53e1eb998c64df6b63a279e226a7a";
 
@@ -67,17 +78,18 @@ public class MainActivity extends AppCompatActivity {
                         stringBuilder.append(line).append("\n");
                     }
                     bufferedReader.close();
-                    return stringBuilder.toString();
+                    //return stringBuilder.toString();
+                    return WeatherData.fromJson(stringBuilder.toString());
                 } finally {
                     urlConnection.disconnect();
                 }
-            } catch (IOException e) {
+            } catch (IOException | JSONException e) {
                 e.printStackTrace();
                 return null;
             }
         }
 
-        @Override
+       /* @Override
         protected void onPostExecute(String s) {
             super.onPostExecute(s);
 
@@ -87,8 +99,67 @@ public class MainActivity extends AppCompatActivity {
                 double temperature = main.getDouble("temp");
                 double celsiusTemperature = temperature - 273.15; // Конвертация из Кельвинов в Цельсии
                 temperatureTextView.setText(String.format("%.2f°C", celsiusTemperature));
+//////////
+                int weatherIconResourceId = getResources().getIdentifier(weatherData.getWeatherIcon(), "drawable", getPackageName());
+                if (weatherIconResourceId != 0) {
+                    weatherIconImageView.setImageResource(weatherIconResourceId);
+                } else {
+                    // Если изображение не найдено, установите изображение по умолчанию
+                    weatherIconImageView.setImageResource(R.drawable.default_weather_icon);
+                }
+                ////////////////
             } catch (JSONException e) {
                 e.printStackTrace();
+            }
+        }*/
+       @Override
+       protected void onPostExecute(WeatherData weatherData) {
+           super.onPostExecute(weatherData);
+
+           if (weatherData != null) {
+               double celsiusTemperature = weatherData.getTemperatureInCelsius();
+               temperatureTextView.setText(String.format("%.2f°C", celsiusTemperature));
+
+               // Загрузка изображения погоды по URL и установка его в ImageView
+               new DownloadImageTask(weatherIconImageView).execute("https://openweathermap.org/img/wn/" + weatherData.getWeatherIcon() + "@2x.png");
+
+              /* // Установка фона в зависимости от температуры
+               if (celsiusTemperature < 10) {
+                   findViewById(android.R.id.content).setBackgroundResource(R.drawable.cold_background);
+               } else {
+                   findViewById(android.R.id.content).setBackgroundResource(R.drawable.default_background);
+               }*/
+           } else {
+               Toast.makeText(MainActivity.this, "Ошибка получения данных о погоде", Toast.LENGTH_LONG).show();
+           }
+       }
+    }
+
+    private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+        ImageView imageView;
+
+        public DownloadImageTask(ImageView imageView) {
+            this.imageView = imageView;
+        }
+
+        protected Bitmap doInBackground(String... urls) {
+            String url = urls[0];
+            Bitmap bitmap = null;
+            try {
+                InputStream in = new java.net.URL(url).openStream();
+                bitmap = BitmapFactory.decodeStream(in);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return bitmap;
+        }
+
+        protected void onPostExecute(Bitmap result) {
+            if (result != null) {
+                imageView.setImageBitmap(result);
+            } else {
+                // Если изображение не загружено, установите изображение по умолчанию
+                imageView.setImageResource(R.drawable.default_weather_icon);
             }
         }
     }
